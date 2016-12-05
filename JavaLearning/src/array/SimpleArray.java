006e79c86eb9a19d66aec5b6d0aab0a161b7dc55
Denis Pergamenstschik <denisp3@verifone.com>
2016-12-05 18:37:32 +0200

    array odd numbers

--- JavaLearning/src/array/SimpleArray.java
+++ JavaLearning/src/array/SimpleArray.java
@@ -1,9 +1,24 @@
 package array;
-
+import javax.swing.*;
 public class SimpleArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub/s
+		// Index variable and border of array:
+		int i,size;
+		
+		//Text to display
+		String text="Array of odd nymbers: \n";
+		//Enter the border of array:
+		size=Integer.parseInt(JOptionPane.showInputDialog("Insert border of array:"));
+		//Creating of array:
+		int[] nums = new int[size];
+		//Enter data to array and creating of text to output:
+		for(i=0;i<size;i++){
+			nums[i]=i*2+1;
+			text+=nums[i]+" ";
+		}
+		// Output message:
+		JOptionPane.showMessageDialog(null, text);
 	}
 
 }
