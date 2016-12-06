53a0053bd0739b8309b79a9f8dda4ec6af902026
Denis Pergamenstschik <denisp3@verifone.com>
2016-12-06 19:08:43 +0200

    first for

--- JavaLearning/src/array/NewSimpleArray.java
+++ JavaLearning/src/array/NewSimpleArray.java
@@ -1,10 +1,23 @@
 package array;
-
+import javax.swing.*;
 public class NewSimpleArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Array size:
+		int n,m;
+		//Output window text:
+		String text="Two-dimensional array of rundom numbers:";
+		//Inserting of array size:
+		n=Integer.parseInt(JOptionPane.showInputDialog("Rows in array"));
+		m=Integer.parseInt(JOptionPane.showInputDialog("Columns in arrow"));
+		//Array creation:
+		int[][] nums = new int [n][m];
+		for(int i=0;i<n;i++){
+			text=text+"*\n";
+			text+="\n";
+		}
+		System.out.println(text);
 	}
+	
 
 }
