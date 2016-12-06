de96aaeffa92406536b4fc915f1bad6f5f239b12
Denis Pergamenstschik <denisp3@verifone.com>
2016-12-06 16:15:10 +0200

    assignment 2

--- JavaLearning/src/array/assignment.java
+++ JavaLearning/src/array/assignment.java
@@ -3,8 +3,16 @@ package array;
 public class assignment {
 
 	public static void main(String[] args) {
-		// 
-
+	/*	int[] first,second;
+		first=new int[10];
+		second=first;
+	*/
+		int[] first, second;
+		first = new int[10];
+		second = new int[5];
+		System.out.println(second.length); 
+		second=first;
+		System.out.println(second.length); 
 	}
 
 }
