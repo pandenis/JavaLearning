6989a247249b3bcfb3bc92166fc6df0f11f336c4
Denis Pergamenstschik <pan.denis@gmail.com>
2016-12-09 19:04:10 +0200

    changed name of class from assignment to Assignment

---
+++ JavaLearning/src/array/Assignment.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+	/*	int[] first,second;
+		first=new int[10];
+		second=first;
+	*/
+		int[] first, second;
+		first = new int[10];
+		second = new int[5];
+		System.out.println(second.length); 
+		second=first;
+		System.out.println(second.length); 
+	}
+
+}
