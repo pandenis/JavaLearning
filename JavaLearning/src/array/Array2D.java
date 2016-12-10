f9b734d932bd220fab8bb2d54f98da33a2b8615b
Denis Pergamenstschik <pan.denis@gmail.com>
2016-12-10 15:47:09 +0200

    2D array example

---
+++ JavaLearning/src/array/Array2D.java
@@ -0,0 +1,16 @@
+package array;
+
+import javax.swing.JOptionPane;
+
+public class Array2D {
+
+	public static void main(String[] args) {
+		int [][] array2D;
+		array2D = new int  [3][];
+		array2D[0]=new int[2];
+		array2D[1]=new int[6];
+		
+		JOptionPane.showMessageDialog(null, array2D);
+	}
+
+}
