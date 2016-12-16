4ad407ead668699284ecc3cbb13dbdec091d22fc
Denis Pergamenstschik <pan.denis@gmail.com>
2016-12-16 20:21:07 +0200

    boolean part1

---
+++ JavaLearning/src/array/CHarArraySorting.java
@@ -0,0 +1,10 @@
+package array;
+
+public class CHarArraySorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- JavaLearning/src/primitiveDataTypes/Boolean.java
+++ JavaLearning/src/primitiveDataTypes/Boolean.java
@@ -1,5 +1,14 @@
 package primitiveDataTypes;
-
+import javax.swing.*;
 public class Boolean {
+	
+	public static void main (String[] args){
+	JOptionPane.showMessageDialog(null,"true && false return "+(true&&false)+"\n"
+			+"true && true return "+(true&&true));
+	JOptionPane.showMessageDialog(null,"false || true return "+(false||true)+"\n"
+			+"true || true return "+(true||true));
+	JOptionPane.showMessageDialog(null, "!true returns "+(!true)+"\n"
+			+"!false returns "+(!false));
+	}
 
 }
