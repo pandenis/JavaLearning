efb360d66f10f40942ee0c134ba5702a2bb06cd5
Denis Pergamenstschik <denisp3@verifone.com>
2016-12-18 15:09:22 +0200

    started Integral Types lesson

--- JavaLearning/src/primitiveDataTypes/IntegralDataTypes.java
+++ JavaLearning/src/primitiveDataTypes/IntegralDataTypes.java
@@ -1,5 +1,15 @@
 package primitiveDataTypes;
-
 public class IntegralDataTypes {
+//	http://pr0java.blogspot.co.il/2015/04/java-byte-short-int-long.html	
+	public static void main(String[] args){
+		/*Error: value out of range:
+		/long ilong = 2147483648;*/
+		long ilong = 2147483647;
+		System.out.println("ilong="+ilong);
+		byte a, b;
+		a=5;
+		b=3;
+		System.out.println("a="+a+"b="+b);
+	}
 
 }
