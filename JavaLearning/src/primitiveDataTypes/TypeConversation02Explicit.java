64282864294d859872db01d0696f53e911ed841e
Denis Pergamenstschik <pan.denis@gmail.com>
2016-12-21 16:49:23 +0200

    New Class Type Conversation explicit

---
+++ JavaLearning/src/primitiveDataTypes/TypeConversation02Explicit.java
@@ -0,0 +1,10 @@
+package primitiveDataTypes;
+
+public class TypeConversation02Explicit {
+// http://pr0java.blogspot.co.il/2015/04/java_23.html
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
