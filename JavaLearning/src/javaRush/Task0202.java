891e957356adae11b041d58f6334f4ce7e856753
Denis Pergamenstschik <pan.denis@gmail.com>
2017-04-01 13:43:48 +0300

    Task0202: create class with details create link to new Object

---
+++ JavaLearning/src/javaRush/Task0202.java
@@ -0,0 +1,18 @@
+package javaRush;
+
+public class Task0202 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person = new Person();
+
+	}
+	public static class Person {
+		String name;
+		int age;
+		int weight;
+		int money;
+		
+	}
+
+}
